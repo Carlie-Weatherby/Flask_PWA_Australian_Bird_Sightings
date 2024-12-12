39ddf1a3d9d0960f86f65940e5d50f4a2143c774
Carlie-Weatherby <acornerofthegalaxy@gmail.com>
2024-12-13 08:56:53 +1100

    database edits

--- shellScript3.sh
+++ shellScript3.sh
@@ -7,3 +7,13 @@ while read -r line; do
 echo $line
 touch $line
 done < files.txt
+
+
+###TEMP
+#mv icons, icons #Renames a directory
+#rm -i daat_source # delete file
+#cd .. # navigate UP one directory
+
+#TODO: Add subfolders bash script stuff
+#cd static
+#mkdir css icons images js
