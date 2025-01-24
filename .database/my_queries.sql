84fe11c26a540a9ce16d2b58e78bf9a3c8897cc1
Carlie-Weatherby <acornerofthegalaxy@gmail.com>
2025-01-24 22:16:42 +1100

    SQL tables for families and genus added and linked

---
+++ .database/my_queries.sql
@@ -0,0 +1,64 @@
+--  CREATE TABLE birdFamilies (
+--     family_id INTEGER PRIMARY KEY AUTOINCREMENT,
+--     family_name_english TEXT NOT NULL,
+--     family_name_latin TEXT NOT NULL,
+--     description TEXT NOT NULL
+--     );
+
+-- *** FAMILIES TABLE - TEMPLATE FOR DATA ENTRY ***
+-- INSERT INTO birdFamilies(family_name_english, family_name_latin, description)
+-- VALUES('name-english', 'name-latin', 'description of family');
+
+-- INSERT INTO birdFamilies(family_name_english, family_name_latin, description)
+-- VALUES ('kingfishers', 'Alcedinidae', 'Rollers, ground rollers, kingfishers');
+
+-- INSERT INTO birdFamilies(family_name_english, family_name_latin, description)
+-- VALUES
+--     ('Cockatoos', 'Cacatuidae', 'Parrots, cockatoos'),]
+--     ('Woodswallows, Butcherbirds, Peltops', 'Artamidae', 'Bristlehead, butcherbirds, woodswallows, Mottled Berryhunter, ioras, cuckooshrikes'),
+--     ('Pelicans', 'Pelecanidae', 'Ibises, spoonbills, herons, Hamerkop, Shoebill, pelicans'),
+--     ('Gulls, Terns, Skimmers', 'Laridae', 'Noddies, skimmers, gulls, terns, skuas, auks');
+
+-- *** COMMANDS I'VE PLAYED WITH (NOT IN OPRDER) ***
+-- DROP TABLE birdFamily;
+-- ALTER TABLE birdFamily RENAME COLUMN family_name TO family_name_english;
+-- ALTER TABLE birdFamily RENAME COLUMN common_family_name TO family_name_latin;
+-- ALTER TABLE birdFamily RENAME TO birdFamilies;
+-- UPDATE birdFamilies SET family_name_latin = 'Artamidae' WHERE family_id = 1003;
+
+-- CREATE TABLE birdGenera (
+--     genus_id INTEGER PRIMARY KEY AUTOINCREMENT,
+--     family_id INTEGER NOT NULL,
+--     --family_id INTEGER REFERENCES birdFamilies(family_id), -- alternate way (inline)
+--     genus_name TEXT NOT NULL,
+--     description TEXT NOT NULL,
+--     FOREIGN KEY (family_id) REFERENCES birdFamilies(family_id)
+--     );
+
+-- *** GENERA TABLE - TEMPLATE FOR DATA ENTRY ***
+-- INSERT INTO birdGenera(family_id, genus_name, description)
+-- VALUES('family id', 'genus name', 'description of genus');
+
+-- INSERT INTO birdGenera(family_id, genus_name, description)
+-- VALUES ('1005', 'Chroicocephalus', 'Masked Gulls and Allies')
+
+INSERT INTO birdGenera(family_id, genus_name, description)
+VALUES
+    ('1002', 'Trichoglossus', 'Rainbow Lorikeets and Allies'),
+    ('1003', 'Cracticus', 'Typical Butcherbirds'),
+    ('1003', 'Strepera', 'Currawongs'),
+    ('1004', 'Pelecanus', 'Pelicans'),
+    ('1002', 'Zanda', 'Australian Cockatoos'),
+    ('1001', 'Dacelo', 'Kookaburras');
+
+
+-- *** QUERIES ***
+-- SELECT birdGenera.*
+-- FROM birdGenera
+-- INNER JOIN birdFamilies ON birdGenera.family_id = birdFamilies.family_id
+-- WHERE birdFamilies.family_name_latin = 'Artamidae';
+
+
+-- SELECT * FROM birdFamilies
+-- WHERE family_id=1002;
+-- LIMIT 3;
