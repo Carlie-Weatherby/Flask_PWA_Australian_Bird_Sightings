9a5202e4708a6fc4cc3e0dfeebcfd93ff73f26a9
Carlie-Weatherby <acornerofthegalaxy@gmail.com>
2025-01-28 01:17:14 +1100

    Various - Incorporated morexisting html into the Jonja2 template - about.html, donate.html

--- main.py
+++ main.py
@@ -8,5 +8,18 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+# Route for the About page
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/search')
+def search_sightings():
+    return render_template('search_sightings.html')
+
+@app.route('/donate')
+def donate():
+    return render_template('donate.html')
+
 if __name__ == '__main__':
     app.run(debug=True, host = '0.0.0.0', port=5000)
